55ab49b90f234f95bceb8b406eb5861f0b6d0a91
Skyw4lkeR77 <93298312+Skyw4lkeR77@users.noreply.github.com>
2021-10-28 10:07:55 +0700

    Create install.py

---
+++ install.py
@@ -0,0 +1,6 @@
+import os,sys
+os.system('figlet install By Skyw4lkeR77')
+os.system('pkg install python')
+os.system('python -m install requests')
+os.system('pip install mechanize')
+os.system('pip install bs4')
